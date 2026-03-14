f940c9846e239d597ce2e9c9902abaf2d0cc196c
Zes Minkey Young <2149291470@qq.com>
2026-03-14 23:03:23 +0800

    chore: 更新schema（AI），bump

--- src/version.ts
+++ src/version.ts
@@ -2,8 +2,8 @@
 
 /// #declaration:global
 
-export const VERSION = 203;
-export const SCHEMA = "https://cdn.jsdelivr.net/npm/kipphi@2.0.2/chartType2.schema.json"
+export const VERSION = 210;
+export const SCHEMA = "https://cdn.jsdelivr.net/npm/kipphi@2.1.0/chartType2.schema.json"
 
 
 /// #enddeclaration
